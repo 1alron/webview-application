343ea65f73a05c5b52a10d40a1937964802a9ca0
Roman Nikolaev <roma.niko393@gmail.com>
2023-09-28 11:29:55 +0300

    Added a handle page navigation

--- app/src/main/java/com/alron/webview/MainActivity.kt
+++ app/src/main/java/com/alron/webview/MainActivity.kt
@@ -3,6 +3,7 @@ package com.alron.webview
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,5 +14,7 @@ class MainActivity : AppCompatActivity() {
 
         myWebView.addJavascriptInterface(WebAppInterface(this), "Android")
         myWebView.settings.javaScriptEnabled = true
+        myWebView.webViewClient = WebViewClient()  // default WebViewClient
+        myWebView.loadUrl("https://berezka-sad.caduk.ru/") // Website URL
     }
 }
