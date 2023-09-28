38e9794cd877b9a414b7c8a647bc02ca89aebb2f
Roman Nikolaev <roma.niko393@gmail.com>
2023-09-28 11:24:51 +0300

    Added WebView object into MainActivity.kt

--- app/src/main/java/com/alron/webview/MainActivity.kt
+++ app/src/main/java/com/alron/webview/MainActivity.kt
@@ -2,10 +2,13 @@ package com.alron.webview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myWebView: WebView = findViewById(R.id.webview)
     }
 }
